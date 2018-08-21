8710901b2aca32747b800ba53a04cb7917c303a3
Omar <ocontreras.sf@gmail.com>
2018-08-21 04:45:40 +0000

    Add script that creates JSON file from user todo list

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# script to gather todo data from an API and write to JSON file
+import sys
+import json
+import requests
+
+
+def get_username(base_url, user_id):
+    """Gets username
+       Args:
+           base_url (str): base url for API
+           user_id (str): user id number
+       Returns: username
+    """
+    response = requests.get(
+        "{}users/{}".format(base_url, user_id))
+    usr_dict = response.json()
+    return usr_dict['username']
+
+
+def get_todo_list(base_url, user_id):
+    """Gets todo list
+       Args:
+           base_url (str): base url for API
+           user_id (str): user id number
+       Returns: list of todo items (dicts)
+    """
+    response = requests.get(
+        "{}users/{}/todos".format(base_url, user_id))
+    return response.json()
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    uname = get_username(base_url, user_id)
+    todo_list = get_todo_list(base_url, user_id)
+
+    with open('{}.json'.format(user_id), 'w') as f:
+        todo_dict = {}
+        todo_dict[sys.argv[1]] = []
+        for todo in todo_list:
+            task = {}
+            task['task'] = todo['title']
+            task['completed'] = todo['completed']
+            task['username'] = uname
+            todo_dict[sys.argv[1]].append(task)
+
+        f.write(json.dumps(todo_dict))
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,3 +1,4 @@
 # API
 * 0-gather_data_from_an_API.py: script to gather data from an API
 * 1-export_to_CSV.py: create CSV file for user to-do list
+* 2-export_to_JSON.py:create JSON file for user to-do list
