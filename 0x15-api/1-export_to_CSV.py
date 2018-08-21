ee2bc040eb6aa0399311cac0e533788d76723850
Omar <ocontreras.sf@gmail.com>
2018-08-21 03:25:40 +0000

    Add script to get todo list by user and write to CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# script to gather todo data from an API and write to CSV file
+import sys
+import csv
+import requests
+
+
+def get_username(base_url, user_id):
+    response = requests.get(
+        "{}users/{}".format(base_url, user_id))
+    usr_dict = response.json()
+    return usr_dict['name']
+
+
+def get_todo_list(base_url, user_id):
+    response = requests.get(
+        "{}users/{}/todos".format(base_url, user_id))
+    return response.json()
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    uname = get_username(base_url, user_id)
+    todo_list = get_todo_list(base_url, user_id)
+
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todo_list:
+            writer.writerow([user_id, uname, todo['completed'], todo['title']])
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,2 +1,3 @@
 # API
 * 0-gather_data_from_an_API.py: script to gather data from an API
+* 1-export_to_CSV.py: create CSV file for user to-do list
