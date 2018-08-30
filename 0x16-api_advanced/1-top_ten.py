d8d9f370b666f8901ba7287badadb1b13fa06eac
Omar <ocontreras.sf@gmail.com>
2018-08-30 08:48:58 +0000

    Add function to return top ten hot posts for subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function that returns the number of subscribers for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Gets top ten posts in subreddit
+       Args:
+           subreddit (str): name of subreddit
+    """
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/hot?limit=10'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    hot_dict = response.json()
+
+    for d in hot_dict['data']['children']:
+        print(d['data']['title'])
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,2 +1,3 @@
 # API advanced
 * 0-subs.py: function that queries the Reddit API and returns the number of subscribers for a given subreddit
+* 1-top_ten.py: function that returns 10 hot posts
