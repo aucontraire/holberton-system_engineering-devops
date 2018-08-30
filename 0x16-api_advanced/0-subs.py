01bfb70a09a60d74472f2a1c995d93c8060ed870
Omar <ocontreras.sf@gmail.com>
2018-08-30 08:09:41 +0000

    Add function that returns number of subreddit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""function that returns the number of subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Gets number of subscribers
+       Args:
+           subreddit (str): name of subreddit
+       Returns:
+           number of subscribers if valid, 0 otherwise
+    """
+    base_url = 'https://www.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/about/.json'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    about_dict = response.json()
+
+    return about_dict['data']['subscribers']
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,1 +1,2 @@
 # API advanced
+* 0-subs.py: function that queries the Reddit API and returns the number of subscribers for a given subreddit
